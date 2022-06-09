e08aee1ff5798902476d48cb0d6293602de9d878
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-06-09 14:25:12 +0200

    fix numero ripetizioni

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -2,7 +2,7 @@
 let userNumber = null;
 let oddNumbers = [];
 // Imposto il ciclo for e pusho nell'array i numeri dispari con l'if statement
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 6; i++) {
   userNumber = prompt("number");
   let number = parseInt(userNumber);
   if (number % 2 !== 0) {
